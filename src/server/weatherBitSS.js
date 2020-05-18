131683a83867bb9cb5d60bf6806c0d24fe1b1008
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-18 15:40:05 -0400

    Successfully sent all weatehr from server to client

--- src/server/weatherBitSS.js
+++ src/server/weatherBitSS.js
@@ -29,49 +29,31 @@ const weatherForcastURL = 'https://api.weatherbit.io/v2.0/forecast/daily';
 // Returns current weatehr and forcast data in a JSON string
 async function getWeatherData(tripDate, lat, lng){
     //const forcastURL = getForcastURL(tripDate);
-    let retVal = null;
+    let forcastData = null;
+    let currentData = null;
     try {
-        //console.log('getForcastData call')
-        //const dateDiff = dayDiff(new Date(tripDate),new Date());
-        //if(dateDiff <= 16)
+
+        //Get forcast data
         await getForcastData(tripDate, lat, lng)
         .then(function(res){
-            //console.log('getWeatherData',res)
-            //console.log('getWeatherData return, has data in it')
-            //console.log(res)
-            retVal =  res;
+            forcastData =  res;
         })
-        // if(forcastURL !== ''){
-        //     forcastURL = forcastURL + '?&lat=' + lat + '&lon=' + lng +'&key=' + weatherKey + '&units=I';
-        //     //console.log(apiURL)
-        //     // Call the GEO API
-        //     await fetch(forcastURL)
-        //     .then(res => res.json())
-        //     .then(function(res) { 
-        //         //Process results then send
-        //         //console.log(JSON.stringify(res))  //Use to get json example
-        //         if('data' in res && res.data.length > 0){
-        //             console.log(weatherBit.processForcastData(res.data));
-        //             return 1;
-        //         }
-        //         else {
-        //             return res;
-        //         }
-        //     })
-        //     .then(function(res){
-        //         //response.send(res);
-        //     })
-        // }
-        // else {
-        //     response.send(''); //Send nothing.  Forcast is not applicable
-        // }
+
+        // Get current weather data
+        await getcurrentData(lat, lng)
+        .then(function(res){
+            currentData =  res;
+        })
+
     } catch (error) {
         console.log('error in getWeather', error);
     }
-    return retVal;
+
+    // Combine the current and forcast data into a single JSON object
+    return {'current': [currentData], 'forcast': [forcastData]};
 }
 
-//Function to get the forcast url if the trip date is within 16 days
+//Function to get the forcast data if the trip date is within 16 days
 //Returns a blank string otherwies
 async function getForcastData(tripDate, lat, lng){
     let forcastData = [];  // Data to return
@@ -81,17 +63,12 @@ async function getForcastData(tripDate, lat, lng){
     if(dateDiff <= 16){
         const forcastURL = weatherForcastURL + '?&lat=' + lat + '&lon=' + lng +'&key=' + weatherKey + '&units=I';
         try{
-            //console.log('fetching forcast')
             await fetch(forcastURL) //Call to the api
             .then(res => res.json())
             .then(async function(res) { 
                 //Process results then return
-                //console.log(JSON.stringify(res))  //Use to get json example
                 if('data' in res && res.data.length > 0){
-                    //console.log('getForcastData',processForcastData(res.data));
-                    //console.log('processForcastData call')
                     forcastData = await processForcastData(res.data);
-                    //forcastData.push(fData);
                 }
             })
         }
@@ -99,15 +76,8 @@ async function getForcastData(tripDate, lat, lng){
             console.log('error in getForcastData', error);
         }
     }
-    //}
-    //console.log('getForcastData',forcastData)
-    //console.log('getForcastData return')
     return forcastData;
 };
-
-// function getCurrentWeatherURL(){
-//     return weatherCurrentURL;
-// }
 
 async function processForcastData(res){
     const forcastDetials = []; // Array to store the place details
@@ -130,16 +100,8 @@ async function processForcastData(res){
                 wIcon: wIcon
             }
         )
-        // }
     }
-    //console.log('processForcastData',JSON.stringify(weatherDetails))
-    // Update the dropdown list for the user to choose
-    //console.log('processForcastData retrun');
     return forcastDetials;
-}
-
-module.exports = {
-    getWeatherData,
 }
 
 //Function to get the difference of 2 dates in days
@@ -151,4 +113,45 @@ function dayDiff(date1,date2){
     const toDays = 1000 * 60 * 60 * 24 //Use to divide the datetime numeric value to convert to days
     return Math.floor((d1-d2)/toDays);
 };
+
+//Function to get the current weather of an area
+async function getcurrentData(lat, lng){
+    let currentData = [];  // Data to return
+    //Get the forcast data if trip is within 16 days
+        const currentURL = weatherCurrentURL + '?&lat=' + lat + '&lon=' + lng +'&key=' + weatherKey + '&units=I';
+        try{
+            await fetch(currentURL) //Call to the api
+            .then(res => res.json())
+            .then(async function(res) { 
+                //Process results then return
+                if('data' in res && res.data.length > 0){
+                    currentData = await processCurrentData(res.data);
+                }
+            })
+        }
+        catch (error){
+            console.log('error in getcurrentData', error);
+        }
+    return currentData;
+};
+
+// Wrapper function to process the api results into a customer JSON object
+async function processCurrentData(res){
+    const retVal = 
+            {
+                windSpd: res[0].wind_spd, 
+                temp: res[0].temp, 
+                feelsLike: res[0].app_temp,
+                clouds: res[0].clouds,
+                desc: res[0].weather.description,
+                wIcon: res[0].weather.icon
+            }
+    return retVal;
+};
+
+
+module.exports = {
+    getWeatherData,
+};
+
 
