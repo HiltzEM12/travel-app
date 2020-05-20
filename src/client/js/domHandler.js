19950210047ee684d02605b796721b09058ae129
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-20 13:42:49 -0400

    Got the cards to strat appearing Cleared out the user fileds when trip is submitted

--- src/client/js/domHandler.js
+++ src/client/js/domHandler.js
@@ -47,28 +47,60 @@ async function travelButtonClick(event) {
 
         //Get a picture of the place and process it
         //Returns a docFrag that will be added to the document
-        const picDocFrag = await  Client.getPicData({
-            displayName: placeDetails.textContent,
-            adminName1: placeDetails.adminName1,
-            countryName: placeDetails.countryName,
-            name: placeDetails.name
-        })
+        try{
+            //console.log('1 await Client.getPicData');
+            let picDocFrag = await Client.getPicData({
+                displayName: placeDetails.textContent,
+                adminName1: placeDetails.adminName1,
+                countryName: placeDetails.countryName,
+                name: placeDetails.name
+            });
+
+            let weatherDocFrag = await Client.getWeatherBitData({
+                date: departure.value +'T00:00' , //Added the time to the date to avoid UTC issues
+                lat: placeDetails.lat,
+                lng: placeDetails.lng
+            });
+
+            createTripBox(picDocFrag, weatherDocFrag);
+            //.then(res => res)
+            //.then(console.log('5 GetPicData Called'))
+            //console.log('5 GetPicData Called',x)
+        }
+        catch(error){
+            //console.log('Error in travelButtonClick',error)
+        }
+        // .then(async function(res){
+            //console.log('picDocFrag',picDocFrag.classList);
+            //console.log('5 GetPicData Called',temp);
+            //createTripBox(temp, null);
+            // const weatherDocFrag = await Client.getWeatherBitData({
+            //     date: departure.value +'T00:00' , //Added the time to the date to avoid UTC issues
+            //     lat: placeDetails.lat,
+            //     lng: placeDetails.lng
+            // })
+            // .then(async function(weatherDocFrag, picDocFrag){
+            //     createTripBox(picDocFrag, weatherDocFrag);
+            // })
+        // })
 
         //console.log(picDocFrag);
         //picBox.appendChild(picDocFrag);
 
         //Get current weather data and forcast of the place and process it
         //Returns a docFrag that will be added to the document
-        const weatherDocFrag = await  Client.getWeatherBitData({
-            date: departure.value +'T00:00' , //Added the time to the date to avoid UTC issues
-            lat: placeDetails.lat,
-            lng: placeDetails.lng
-        })
+        // const weatherDocFrag = await  Client.getWeatherBitData({
+        //     date: departure.value +'T00:00' , //Added the time to the date to avoid UTC issues
+        //     lat: placeDetails.lat,
+        //     lng: placeDetails.lng
+        // })
         // console.log('latlon',placeDD.value)
         // console.log('date',departure.value)
 
         //Create trip doc frag and add it
-        createTripBox(picDocFrag, weatherDocFrag);
+        // createTripBox(picDocFrag, weatherDocFrag);
+        //Clear out the data fields to prepare for another trip
+        clearFields();
     }
     else{
         alert('Enter a place and a date')
@@ -80,18 +112,25 @@ const tripGrid = document.getElementById('trip-grid-div');
 
 // Function to create the trip box docfrag to add to the document
 function createTripBox(picDocFrag, weatherDocFrag){
-    tripGrid.appendChild(picDocFrag)
-    // const docFrag = document.createDocumentFragment(); //Docfrag to add to then append to the doc
-    // const mainDiv = document.createElement('div'); //Main trip box div
-    // mainDiv.classList.add('trip-box');
-    // mainDiv.appendChild(picDocFrag); //Add the picture
+    //tripGrid.appendChild(picDocFrag)
+    const docFrag = document.createDocumentFragment(); //Docfrag to add to then append to the doc
+    const mainDiv = document.createElement('div'); //Main trip box div
+    mainDiv.classList.add('trip-box');
+    mainDiv.appendChild(picDocFrag); //Add the picture
 
-    // docFrag.appendChild(mainDiv);
+    docFrag.appendChild(mainDiv);
 
-    // tripGrid.appendChild(docFrag);
+    tripGrid.appendChild(docFrag);
 
 }
 
 export { 
     setDates
 };
+
+// Clears out the user input fields to prepare for another trip
+function clearFields(){
+    placeDD.innerHTML = '';
+    placeNameField.value = '';
+    departure.value = '';
+}
--- src/client/js/pixabay.js
+++ src/client/js/pixabay.js
@@ -2,13 +2,17 @@
 
 // const picBox = document.getElementById("test-pic");  //Chnage this when it goes live.  For testing only
 
+// Place dropdown
+const placeDD = document.getElementById('place-dropdown');
+
 // Function to get the data.
 // Argument is a JSON string with search terms
 async function getPicData(term){
     let docFrag = document.createDocumentFragment(); // Document frag to return
     //Post to the server with the search term as an argument
     try{
-    fetch('/pic', {
+    //console.log('Fetch pic url')
+    let res = await fetch('/pic', {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
@@ -17,23 +21,27 @@ async function getPicData(term){
         body: JSON.stringify({'text' :term}),
         //body: JSON.stringify({'text': place}), // body data type must match 'Content-Type' header        
     })
-    .then(res => res.json())
-    .then(function(res){
+    //.then(res => res.json())
+    res = await res.json();
+    //.then(async function(res){
         //console.log('UpdateWeather Call');
         //Client.updateWeather(res);
         //console.log('getPicData', res)
-        docFrag = processPicUrl(res);
-    })
+        //console.log('2 await processPicUrl',docFrag)
+    docFrag = await processPicUrl(res);
+        //console.log('4 return processPicUrl',docFrag)
+    //})
         //await console.log(fetch('/geo')))  //Process the json
     }
     catch(error){
         console.log('error in getPicData: ', error)
     }
+    
     return docFrag;
 };
 
 
-function processPicUrl(url){
+async function processPicUrl(url){
     //console.log('in process Pic URL',url)
     //Example
     // <figure>
@@ -45,27 +53,24 @@ function processPicUrl(url){
 
         //picBox.innerHTML = '';  //Clear out old.  Only needed in testin
 
+        const place = placeDD.options[placeDD.selectedIndex].text;
+
         const fig = document.createElement('figure');
         const figCap = document.createElement('figureCaption');
         figCap.classList.add('holder');
-        figCap.textContent = 'test place'
+        figCap.textContent = `Trip to: ${place}`;
         fig.appendChild(figCap);
         const img = document.createElement('img');
         img.classList.add('trip-pic');
-        img.alt = 'test place';
+        img.alt = place;
         img.src = url;
         fig.appendChild(img);
         docFrag.appendChild(fig);
         //picBox.appendChild(docFrag);
+        //console.log('3 createDocumentFragment docFrag',docFrag)
     }
     return docFrag;
 }
-
-
-
-
-
-
 
 export { 
     getPicData
