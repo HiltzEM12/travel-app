84f48b2cc28f5dd86dda243f9ae1f8458b0b4780
Eric Hiltz <eric.m.hiltz@gmail.com>
2020-05-22 15:07:39 -0400

    successfully created multiple html files and was able to package them with webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,9 +3,36 @@ const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// We need Nodes fs module to read directory contents
+// const fs = require('fs');
+
+//Taken from https://extri.co/2017/07/11/generating-multiple-html-pages-with-htmlwebpackplugin/
+// Our function that generates our html plugins
+// function generateHtmlPlugins(templateDir) {
+//     // Read files in template directory
+//     const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
+//     return templateFiles.map(item => {
+//         // Split names and extension
+//         const parts = item.split('.')
+//         const name = parts[0]
+//         const extension = parts[1]
+//         // Create new HTMLWebpackPlugin with options
+//         return new HTMLWebpackPlugin({
+//             filename: `${name}.html`,
+//             template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`)
+//         })
+//     })
+// }
+
+// Call our function on our views directory.
+// const htmlPlugins = generateHtmlPlugins('./src/client/views')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -18,37 +45,44 @@ module.exports = {
         library: 'Client'
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
                 test: /\.scss$/,
-                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader',],
+                use: ['file-loader', ],
             }
         ]
     },
     plugins: [
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        }),
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin({filename: '[name].css'}),
-        new WorkboxPlugin.GenerateSW(),
-    ]
+            new CleanWebpackPlugin({
+                // Simulate the removal of files
+                dry: true,
+                // Write Logs to Console
+                verbose: true,
+                // Automatically remove all unused webpack assets on rebuild
+                cleanStaleWebpackAssets: true,
+                protectWebpackAssets: false
+            }),
+            new HtmlWebPackPlugin({
+                template: "./src/client/views/index.html",
+                filename: "./index.html",
+            }),
+            new HtmlWebPackPlugin({
+                template: "./src/client/views/test-page.html",
+                filename: "./test-page.html",
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css'
+            }),
+            new WorkboxPlugin.GenerateSW(),
+        ] // We join our htmlPlugin array to the end
+        // of our webpack plugins array.
+        // .concat(htmlPlugins)
 }
